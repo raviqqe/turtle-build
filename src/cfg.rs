dd1f3536e181d66ddd91722d263f5144ecbbf0bc
Yota Toyama <raviqqe@gmail.com>
2022-01-07 03:30:28 +0000

    Define build

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,7 +1,9 @@
+mod build;
 mod module;
 mod rule;
 mod variable_definition;
 
+pub use build::*;
 pub use module::*;
 pub use rule::*;
 pub use variable_definition::*;
--- src/cfg/module.rs
+++ src/cfg/module.rs
@@ -1,16 +1,22 @@
-use super::{Rule, VariableDefinition};
+use super::{Build, Rule, VariableDefinition};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Module {
     variable_definitions: Vec<VariableDefinition>,
     rules: Vec<Rule>,
+    builds: Vec<Build>,
 }
 
 impl Module {
-    pub fn new(variable_definitions: Vec<VariableDefinition>, rules: Vec<Rule>) -> Self {
+    pub fn new(
+        variable_definitions: Vec<VariableDefinition>,
+        rules: Vec<Rule>,
+        builds: Vec<Build>,
+    ) -> Self {
         Self {
             variable_definitions,
             rules,
+            builds,
         }
     }
 
@@ -20,5 +26,9 @@ impl Module {
 
     pub fn rules(&self) -> &[Rule] {
         &self.rules
+    }
+
+    pub fn builds(&self) -> &[Build] {
+        &self.builds
     }
 }

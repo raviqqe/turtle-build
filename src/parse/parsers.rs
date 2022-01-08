4b16022fac5a64b543db56681d76009f27bff101
Yota Toyama <raviqqe@gmail.com>
2022-01-08 02:19:08 +0000

    Test blanks

--- src/parse/parsers.rs
+++ src/parse/parsers.rs
@@ -1,13 +1,16 @@
 use super::stream::Stream;
 use crate::cfg::{Module, VariableDefinition};
 use combine::{
-    choice, eof, many, none_of,
-    parser::char::{alpha_num, char, letter, newline, space, string},
+    choice, eof, many, many1, none_of, one_of, optional,
+    parser::char::{alpha_num, char, letter, newline, string},
     value, Parser,
 };
 
 pub fn module<'a>() -> impl Parser<Stream<'a>, Output = Module> {
-    (blank(), many(variable_definition()))
+    (
+        optional(line_break()),
+        many(variable_definition().skip(line_break())),
+    )
         .skip(eof())
         .map(|(_, variable_definitions)| Module::new(variable_definitions, vec![], vec![], vec![]))
 }
@@ -27,7 +30,7 @@ fn identifier<'a>() -> impl Parser<Stream<'a>, Output = String> {
             choice((letter(), char('_'))),
             many(choice((alpha_num(), char('_')))),
         )
-            .map(|(head, tail): (char, String)| [head.into(), tail].concat()),
+            .map(|(head, tail): (_, String)| [head.into(), tail].concat()),
     )
 }
 
@@ -42,17 +45,61 @@ fn token<'a, O, P: Parser<Stream<'a>, Output = O>>(
 }
 
 fn blank<'a>() -> impl Parser<Stream<'a>, Output = ()> {
-    many::<Vec<_>, _, _>(space()).with(value(()))
+    many::<Vec<_>, _, _>(one_of([' ', '\t', '\r'])).with(value(()))
 }
 
-fn blank_lines<'a>() -> impl Parser<Stream<'a>, Output = ()> {
-    many::<Vec<_>, _, _>(newline()).with(value(()))
+fn line_break<'a>() -> impl Parser<Stream<'a>, Output = ()> {
+    many1::<Vec<_>, _, _>((blank(), newline())).with(value(()))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::parse::stream::stream;
+
+    #[test]
+    fn parse_module() {
+        assert_eq!(
+            module().parse(stream("")).unwrap().0,
+            Module::new(vec![], vec![], vec![], vec![])
+        );
+        assert_eq!(
+            module().parse(stream("x = 42\n")).unwrap().0,
+            Module::new(
+                vec![VariableDefinition::new("x", "42")],
+                vec![],
+                vec![],
+                vec![]
+            )
+        );
+        assert_eq!(
+            module().parse(stream("x = 1\ny = 2\n")).unwrap().0,
+            Module::new(
+                vec![
+                    VariableDefinition::new("x", "1"),
+                    VariableDefinition::new("y", "2"),
+                ],
+                vec![],
+                vec![],
+                vec![]
+            )
+        );
+    }
+
+    #[test]
+    fn parse_variable_definition() {
+        assert_eq!(
+            variable_definition().parse(stream("x = 42")).unwrap().0,
+            VariableDefinition::new("x", "42")
+        );
+        assert_eq!(
+            variable_definition()
+                .parse(stream("foo = 1 + 1"))
+                .unwrap()
+                .0,
+            VariableDefinition::new("foo", "1 + 1")
+        );
+    }
 
     #[test]
     fn parse_string_literal() {
@@ -92,5 +139,27 @@ mod tests {
             identifier().parse(stream("_foo")).unwrap().0,
             "_foo".to_string()
         );
+    }
+
+    #[test]
+    fn parse_blank() {
+        assert!(blank().skip(eof()).parse(stream("")).is_ok());
+        assert!(blank().skip(eof()).parse(stream(" ")).is_ok());
+        assert!(blank().skip(eof()).parse(stream("\t")).is_ok());
+        assert!(blank().skip(eof()).parse(stream("\r")).is_ok());
+        assert!(blank().skip(eof()).parse(stream("  ")).is_ok());
+        assert!(blank().skip(eof()).parse(stream(" \t")).is_ok());
+        assert!(blank().skip(eof()).parse(stream("\n")).is_err());
+        assert!(blank().skip(eof()).parse(stream(" \n")).is_err());
+    }
+
+    #[test]
+    fn parse_line_break() {
+        assert!(line_break().skip(eof()).parse(stream("")).is_err());
+        assert!(line_break().skip(eof()).parse(stream("\n")).is_ok());
+        assert!(line_break().skip(eof()).parse(stream(" \n")).is_ok());
+        assert!(line_break().skip(eof()).parse(stream("  \n")).is_ok());
+        assert!(line_break().skip(eof()).parse(stream("\n\n")).is_ok());
+        assert!(line_break().skip(eof()).parse(stream("\n ")).is_err());
     }
 }

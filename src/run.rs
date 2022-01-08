7046842aeea801b77bb6696725cc81500365d98c
Yota Toyama <raviqqe@gmail.com>
2022-01-08 20:26:12 +0000

    Wait for inputs

--- src/run.rs
+++ src/run.rs
@@ -1,31 +1,51 @@
 mod error;
 
-use crate::ir::Configuration;
-use error::InfrastructureError;
+use crate::ir::{Build, Configuration};
+use error::RunError;
+use futures::future::{FutureExt, Shared};
+use futures::stream::FuturesUnordered;
 use std::collections::HashMap;
-use std::error::Error;
+use std::future::Future;
+use std::pin::Pin;
 use tokio::io::AsyncWriteExt;
 use tokio::spawn;
 use tokio::{io::stderr, process::Command};
 
-pub async fn run(configuration: &Configuration) -> Result<(), Box<dyn Error>> {
+type BuildFuture = Shared<Pin<Box<dyn Future<Output = Result<(), RunError>>>>>;
+
+pub async fn run(configuration: &Configuration) -> Result<(), RunError> {
     let mut builds = HashMap::new();
 
-    for (_name, build) in configuration.outputs() {
-        let rule = build.rule().clone();
-
-        if !builds.contains_key(build.id()) {
-            builds.insert(
-                build.id(),
-                spawn(async move { run_command(rule.command()).await }),
-            );
-        }
+    for (_, build) in configuration.outputs() {
+        run_build(&mut builds, build);
     }
 
     Ok(())
 }
 
-async fn run_command(command: &str) -> Result<(), Box<dyn Error + Send + Sync>> {
+fn run_build(builds: &mut HashMap<String, BuildFuture>, build: &Build) -> BuildFuture {
+    if let Some(future) = builds.get(build.id()) {
+        return future.clone();
+    }
+
+    let rule = build.rule().clone();
+    let handle = spawn(async move { run_command(rule.command()).await });
+    let boxed: Pin<Box<dyn Future<Output = _>>> = Box::pin(async move { handle.await? });
+    let future = boxed.shared();
+
+    builds.insert(build.id().into(), future.clone());
+
+    future
+}
+
+// TODO
+// fn run_input_builds(
+//     configuration: &Configuration,
+//     builds: &mut HashMap<String, BuildFuture>,
+// ) -> FuturesUnordered<BuildFuture> {
+// }
+
+async fn run_command(command: &str) -> Result<(), RunError> {
     let output = Command::new("sh")
         .arg("-e")
         .arg("-c")
@@ -39,6 +59,6 @@ async fn run_command(command: &str) -> Result<(), Box<dyn Error + Send + Sync>> 
         stderr().write_all(&output.stdout).await?;
         stderr().write_all(&output.stdout).await?;
 
-        Err(InfrastructureError::ChildExit(output.status.code()).into())
+        Err(RunError::ChildExit(output.status.code()).into())
     }
 }
--- src/run/error.rs
+++ src/run/error.rs
@@ -2,15 +2,17 @@ use std::{
     error::Error,
     fmt::{self, Display, Formatter},
 };
+use tokio::{io, task::JoinError};
 
 #[derive(Clone, Debug)]
-pub enum InfrastructureError {
+pub enum RunError {
     ChildExit(Option<i32>),
+    Other(String),
 }
 
-impl Error for InfrastructureError {}
+impl Error for RunError {}
 
-impl Display for InfrastructureError {
+impl Display for RunError {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
         match self {
             Self::ChildExit(Some(code)) => {
@@ -19,6 +21,21 @@ impl Display for InfrastructureError {
             Self::ChildExit(None) => {
                 write!(formatter, "child process exited without status code")
             }
+            Self::Other(message) => {
+                write!(formatter, "{}", message)
+            }
         }
+    }
+}
+
+impl From<io::Error> for RunError {
+    fn from(error: io::Error) -> Self {
+        Self::Other(format!("{}", &error))
+    }
+}
+
+impl From<JoinError> for RunError {
+    fn from(error: JoinError) -> Self {
+        Self::Other(format!("{}", &error))
     }
 }

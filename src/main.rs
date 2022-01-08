44693262a48b82d0e42ae543bb0fadb27add2b48
Yota Toyama <raviqqe@gmail.com>
2022-01-08 03:01:28 +0000

    Parse configuration

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 mod cfg;
 mod parse;
 
+use cfg::Module;
+use parse::parse;
+use std::error::Error;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn Error>> {
+    read_configuration().await?;
+
+    Ok(())
+}
+
+async fn read_configuration() -> Result<Module, Box<dyn Error>> {
+    let mut source = "".into();
+
+    File::open("main.ninja")
+        .await?
+        .read_to_string(&mut source)
+        .await?;
+
+    Ok(parse(&source)?)
 }

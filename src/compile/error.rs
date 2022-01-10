c4de2ad699b5023fe1f66182a9dbfd8fc92558d2
Yota Toyama <raviqqe@gmail.com>
2022-01-10 03:36:59 +0000

    Handle misssing rules and variables

---
+++ src/compile/error.rs
@@ -0,0 +1,30 @@
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+    path::PathBuf,
+};
+
+#[derive(Clone, Debug)]
+pub enum CompileError {
+    ModuleNotFound(PathBuf),
+    RuleNotFound(String),
+    VariableNotFound(String),
+}
+
+impl Error for CompileError {}
+
+impl Display for CompileError {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::ModuleNotFound(path) => {
+                write!(formatter, "module \"{}\" not found", path.display())
+            }
+            Self::RuleNotFound(rule) => {
+                write!(formatter, "rule \"{}\" not found", rule)
+            }
+            Self::VariableNotFound(variable) => {
+                write!(formatter, "variable \"{}\" not found", variable)
+            }
+        }
+    }
+}

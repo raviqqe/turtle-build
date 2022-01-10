14426956c26dd3a299a7cd09a412075a6734ec46
Yota Toyama <raviqqe@gmail.com>
2022-01-10 00:32:14 -0800

    Implicit dependencies (#35)
    * Parse implicit dependencies
    
    * Compile implicit dependencies
    
    * Update readme

--- src/ast/build.rs
+++ src/ast/build.rs
@@ -3,22 +3,28 @@ use super::VariableDefinition;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Build {
     outputs: Vec<String>,
+    implicit_outputs: Vec<String>,
     rule: String,
     inputs: Vec<String>,
+    implicit_inputs: Vec<String>,
     variable_definitions: Vec<VariableDefinition>,
 }
 
 impl Build {
     pub fn new(
         outputs: Vec<String>,
+        implicit_outputs: Vec<String>,
         rule: impl Into<String>,
         inputs: Vec<String>,
+        implicit_inputs: Vec<String>,
         variable_definitions: Vec<VariableDefinition>,
     ) -> Self {
         Self {
             outputs,
+            implicit_outputs,
             rule: rule.into(),
             inputs,
+            implicit_inputs,
             variable_definitions,
         }
     }
@@ -27,12 +33,20 @@ impl Build {
         &self.outputs
     }
 
+    pub fn implicit_outputs(&self) -> &[String] {
+        &self.implicit_outputs
+    }
+
     pub fn rule(&self) -> &str {
         &self.rule
     }
 
     pub fn inputs(&self) -> &[String] {
         &self.inputs
+    }
+
+    pub fn implicit_inputs(&self) -> &[String] {
+        &self.implicit_inputs
     }
 
     pub fn variable_definitions(&self) -> &[VariableDefinition] {

ab461403ad6c804fc5bfd34fa63321972fe62800
Yota Toyama <raviqqe@gmail.com>
2022-01-11 14:47:58 +0000

    Parse dynamic module

--- src/parse.rs
+++ src/parse.rs
@@ -2,10 +2,20 @@ mod error;
 mod parsers;
 mod stream;
 
-use self::{error::ParseError, parsers::module, stream::stream};
-use crate::ast::Module;
+use self::{
+    error::ParseError,
+    parsers::{dynamic_module, module},
+    stream::stream,
+};
+use crate::ast::{DynamicModule, Module};
 use combine::Parser;
 
 pub fn parse(source: &str) -> Result<Module, ParseError> {
     Ok(module().parse(stream(source)).map(|(module, _)| module)?)
+}
+
+pub fn parse_dynamic(source: &str) -> Result<DynamicModule, ParseError> {
+    Ok(dynamic_module()
+        .parse(stream(source))
+        .map(|(module, _)| module)?)
 }

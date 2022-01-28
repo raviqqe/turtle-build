6d1d1e0d5ef0aa76d882586fc2cabea1e79b008f
Yota Toyama <raviqqe@gmail.com>
2022-01-28 02:24:15 +0000

    Compile source map

--- src/compile/global_state.rs
+++ src/compile/global_state.rs
@@ -1,9 +1,10 @@
 use crate::ir::Build;
-use fnv::FnvHashMap;
-use std::{collections::HashSet, sync::Arc};
+use fnv::{FnvHashMap, FnvHashSet};
+use std::sync::Arc;
 
 #[derive(Clone, Debug)]
 pub struct GlobalState {
     pub outputs: FnvHashMap<String, Arc<Build>>,
-    pub default_outputs: HashSet<String>,
+    pub default_outputs: FnvHashSet<String>,
+    pub source_map: FnvHashMap<String, String>,
 }

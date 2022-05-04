f754fb29128cc024011152f498eba9771abd754d
Yota Toyama <raviqqe@gmail.com>
2022-05-04 15:13:49 +0000

    Allow empty variable values

--- src/parse/parsers.rs
+++ src/parse/parsers.rs
@@ -40,9 +40,12 @@ fn statement<'a>() -> impl Parser<Stream<'a>, Output = Statement> {
 }
 
 fn variable_definition<'a>() -> impl Parser<Stream<'a>, Output = VariableDefinition> {
-    (attempt(identifier().skip(sign("="))), string_line())
+    (
+        attempt(identifier().skip(sign("="))),
+        optional(string_line()),
+    )
         .skip(line_break())
-        .map(|(name, value)| VariableDefinition::new(name, value))
+        .map(|(name, value)| VariableDefinition::new(name, value.unwrap_or_default()))
 }
 
 fn dynamic_module_version<'a>() -> impl Parser<Stream<'a>, Output = String> {

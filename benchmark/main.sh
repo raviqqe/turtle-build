c4b0b8223efad483b25b2f2cdb555b33535a9333
Yota Toyama <raviqqe@gmail.com>
2022-11-08 09:04:38 -0800

    Fix

--- benchmark/main.sh
+++ benchmark/main.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+rule_count=100
+build_count=10000
+
 print_rule() (
   echo rule $1
   echo "" command = cp $in $out
@@ -9,13 +12,30 @@ print_rule() (
 )
 
 print_build() (
-  echo build $2: $1 $3
+  echo build $3: $1 $2
 )
 
 print_default() (
   echo default $1
 )
 
-for command in $(seq 0 100000); do
+mkdir -p tmp
 
-done
+for index in $(seq 0 $rule_count); do
+  rule=rule$index
+
+  print_rule $rule
+
+  for index in $(seq 0 $build_count); do
+    input=input$index
+    output=output$index
+
+    print_build $rule $input $output
+    print_default $output
+  done
+done >tmp/build.ninja
+
+(
+  cd tmp
+  hyperfine ninja turtle
+)

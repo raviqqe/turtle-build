9dadec24ac034c454ada05ba876d35fe9891a9ac
Yota Toyama <raviqqe@gmail.com>
2022-11-07 23:04:03 -0800

    Fix more

--- src/ast/statement.rs
+++ src/ast/statement.rs
@@ -1,46 +1,46 @@
 use super::{Build, DefaultOutput, Include, Rule, Submodule, VariableDefinition};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Statement {
-    Build(Build),
-    Default(DefaultOutput),
+pub enum Statement<'a> {
+    Build(Build<'a>),
+    Default(DefaultOutput<'a>),
     Include(Include),
-    Rule(Rule),
-    Submodule(Submodule),
-    VariableDefinition(VariableDefinition),
+    Rule(Rule<'a>),
+    Submodule(Submodule<'a>),
+    VariableDefinition(VariableDefinition<'a>),
 }
 
-impl From<Build> for Statement {
+impl<'a> From<Build<'a>> for Statement<'a> {
     fn from(build: Build) -> Self {
         Self::Build(build)
     }
 }
 
-impl From<DefaultOutput> for Statement {
+impl<'a> From<DefaultOutput<'a>> for Statement<'a> {
     fn from(default: DefaultOutput) -> Self {
         Self::Default(default)
     }
 }
 
-impl From<Include> for Statement {
+impl<'a> From<Include<'a>> for Statement<'a> {
     fn from(include: Include) -> Self {
         Self::Include(include)
     }
 }
 
-impl From<Rule> for Statement {
+impl<'a> From<Rule<'a>> for Statement<'a> {
     fn from(rule: Rule) -> Self {
         Self::Rule(rule)
     }
 }
 
-impl From<Submodule> for Statement {
+impl<'a> From<Submodule<'a>> for Statement<'a> {
     fn from(submodule: Submodule) -> Self {
         Self::Submodule(submodule)
     }
 }
 
-impl From<VariableDefinition> for Statement {
+impl<'a> From<VariableDefinition<'a>> for Statement<'a> {
     fn from(variable_definition: VariableDefinition) -> Self {
         Self::VariableDefinition(variable_definition)
     }

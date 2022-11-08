483f33b15679a2391522c604fad33095ff73dcf3
Yota Toyama <raviqqe@gmail.com>
2022-11-08 08:53:43 -0800

    Fix

--- benchmark/main.sh
+++ benchmark/main.sh
@@ -2,14 +2,19 @@
 
 set -e
 
-print_rule() {
+print_rule() (
   echo rule $1
   echo "" command = : $in $out
-}
+  echo "" description = hahahaha
+)
 
-print_rule() {
+print_build() (
   echo rule foo
-}
+)
+
+print_default() (
+  echo default $1
+)
 
 for command in $(seq 0 100000); do
 

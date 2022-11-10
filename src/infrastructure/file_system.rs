33b845575463fbb39c5b628ec1a82068f44f4b4c
Yota Toyama <raviqqe@gmail.com>
2022-11-09 18:27:06 -0800

    Fix

--- src/infrastructure/file_system.rs
+++ src/infrastructure/file_system.rs
@@ -2,6 +2,7 @@ use async_trait::async_trait;
 use std::error::Error;
 use std::fmt::{self, Display};
 use std::fmt::{Debug, Formatter};
+use std::fs::Metadata;
 use std::io;
 use std::path::{Path, PathBuf};
 use tokio::fs::{self, File};
@@ -10,6 +11,7 @@ use tokio::io::AsyncReadExt;
 #[async_trait]
 pub trait FileSystem: Debug {
     async fn read_file(&self, path: &Path, buffer: &mut Vec<u8>) -> Result<(), Box<dyn Error>>;
+    async fn metadata(&self, path: &Path) -> Result<Metadata, Box<dyn Error>>;
     async fn create_directory(&self, path: &Path) -> Result<(), Box<dyn Error>>;
     async fn canonicalize_path(&self, path: &Path) -> Result<PathBuf, Box<dyn Error>>;
 }
@@ -34,6 +36,12 @@ impl FileSystem for OsFileSystem {
             .map_err(|error| OsFileSystemError::new(error, path))?;
 
         Ok(())
+    }
+
+    async fn metadata(&self, path: &Path) -> Result<Metadata, Box<dyn Error>> {
+        Ok(fs::metadata(path)
+            .await
+            .map_err(|error| OsFileSystemError::new(error, path))?)
     }
 
     async fn create_directory(&self, path: &Path) -> Result<(), Box<dyn Error>> {

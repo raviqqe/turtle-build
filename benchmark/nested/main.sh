bf21f63616d1565bed5124fae10bd8f634fb7b9c
Yota Toyama <raviqqe@gmail.com>
2022-11-11 11:22:08 -0800

    Fix

--- benchmark/nested/main.sh
+++ benchmark/nested/main.sh
@@ -3,7 +3,8 @@
 set -e
 
 rule_count=10
-build_count=100
+input_count=100
+subninja_count=10
 
 print_rule() (
   echo rule $1
@@ -29,7 +30,7 @@ for index in $(seq 0 $rule_count); do
 
   print_rule $rule
 
-  for index in $(seq 0 $build_count); do
+  for index in $(seq 0 $input_count); do
     input=${rule}_$index.in
     output=${rule}_$index.out
 
@@ -38,6 +39,25 @@ for index in $(seq 0 $rule_count); do
     print_default $output
   done
 done >build.ninja
+
+for index in $(seq 0 $subninja_count); do
+  subninja=subninja$index
+  subninja_file=$subninja.ninja
+
+  for index in $(seq 0 $rule_count); do
+    rule=rule$index
+
+    for index in $(seq 0 $input_count); do
+      input=${rule}_$index.in
+      output=${subninja}_${rule}_$index.out
+
+      print_build $rule $input $output
+      print_default $output
+    done
+  done >$subninja_file
+
+  echo subninja $subninja_file >>build.ninja
+done
 
 cargo install hyperfine
 hyperfine -p ../clean.sh ninja turtle

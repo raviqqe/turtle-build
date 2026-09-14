6cbd46f99adfa4fad77dc010d7c7a8fe161b813b
Yota Toyama <raviqqe@gmail.com>
2026-09-14 00:14:28 -0700

    Fix

--- tools/acceptance/cmake/main.sh
+++ tools/acceptance/cmake/main.sh
@@ -4,9 +4,4 @@ set -e
 
 git clone --depth 1 --branch v4.4.3 https://github.com/Kitware/CMake .
 
-cmake \
-  -G Ninja \
-  -S . \
-  -B build \
-  -D CMAKE_BUILD_TYPE=Release \
-  -D BUILD_TESTING=OFF
+cmake -G Ninja -S . -B build -D CMAKE_BUILD_TYPE=Release -D BUILD_TESTING=OFF
--- tools/acceptance/llvm/main.sh
+++ tools/acceptance/llvm/main.sh
@@ -6,9 +6,4 @@ set -e
 git clone --depth 1 --branch llvmorg-23.1.1 --filter blob:none --sparse https://github.com/llvm/llvm-project .
 git sparse-checkout set cmake libc llvm third-party
 
-cmake \
-  -G Ninja \
-  -S llvm \
-  -B build \
-  -D CMAKE_BUILD_TYPE=Release \
-  -D LLVM_TARGETS_TO_BUILD=host
+cmake -G Ninja -S llvm -B build -D CMAKE_BUILD_TYPE=Release -D LLVM_TARGETS_TO_BUILD=host

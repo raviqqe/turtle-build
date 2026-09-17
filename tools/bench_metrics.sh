8e034567dfb7d90d4e31045d6aa3b54745334a48
Yota Toyama <raviqqe@gmail.com>
2026-09-16 22:52:05 -0700

    Remove percentage units from benchmark reports (#762)

--- tools/bench_metrics.sh
+++ tools/bench_metrics.sh
@@ -6,11 +6,11 @@ cd $(dirname $0)/../bench
 
 jq -n --arg os $1 '{
   key: $os,
-  name: "build time relative to Ninja on \($os) (%)",
+  name: "build time relative to Ninja on \($os)",
   metrics: [
     inputs.results | INDEX(.parameters.tool) | {
       key: (.turtle.command | capture("\\((?<name>.+)\\)").name),
-      value: (.turtle.mean / .ninja.mean * 100)
+      value: (.turtle.mean / .ninja.mean)
     }
   ]
 }' */tmp/*.json

9d3707cd58f5023e25fff099683cbd375606faad
Yota Toyama <raviqqe@gmail.com>
2026-09-21 20:03:30 -0700

    Add todo comment

--- src/run.rs
+++ src/run.rs
@@ -477,6 +477,7 @@ async fn write_description<'a>(
     context: &'a RunContext,
     rule: &Rule,
 ) -> Result<MutexGuard<'a, dyn Console + Send + Sync>, BuildError> {
+    // TODO Make console lock free.
     let mut console = context.build().console().lock().await;
 
     if let Some(description) = rule.description() {
